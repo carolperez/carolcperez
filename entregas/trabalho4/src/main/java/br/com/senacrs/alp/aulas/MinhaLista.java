f2282dd52a29a2f6bc9108547ef259a61e8c3bb3
631220002 <631220002@Sala603-25.fspoaeduc.com.br>
2013-03-25 21:49:20 -0300

    final trabalho 4

--- entregas/trabalho4/src/main/java/br/com/senacrs/alp/aulas/MinhaLista.java
+++ entregas/trabalho4/src/main/java/br/com/senacrs/alp/aulas/MinhaLista.java
@@ -1,70 +1,109 @@
 package br.com.senacrs.alp.aulas;
 
-public class MinhaLista<T> implements Lista <T>{
-	
+public class MinhaLista<T> implements Lista<T> {
+
 	private Nodo<T> inicio = new Nodo<T>();
 
 	@Override
 	public void adicionarFinal(T valor) {
-		// TODO Auto-generated method stub
-		
+
+		adicionarPosicao(obterTamanho(), valor);
+
 	}
 
 	@Override
 	public void adicionarInicio(T valor) {
-		// TODO Auto-generated method stub
-		
+
+		adicionarPosicao(0, valor);
+
 	}
 
 	@Override
 	public void adicionarPosicao(int posicao, T valor) {
-		// TODO Auto-generated method stub
-		
+
+		Nodo<T> nodo =	null;
+		Nodo<T> novo =	new Nodo<T>();
+		nodo = obterNodoPosicao(posicao - 1);
+		novo.proximo = nodo.proximo;
+		nodo.proximo = novo;
+		novo.conteudo = valor;
+
 	}
 
 	@Override
 	public T obterPrimeiro() {
-		// TODO Auto-generated method stub
-		return null;
+
+		Nodo<T> nodo =	null;
+		nodo = obterNodoPosicao(0);
+		return nodo.conteudo;
+
 	}
 
 	@Override
 	public T obterUltimo() {
-		// TODO Auto-generated method stub
-		return null;
+
+		int tamanho = obterTamanho();
+		T resultado = obterPosicao(tamanho - 1);
+		return resultado;
+
 	}
 
 	@Override
 	public T obterPosicao(int posicao) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Nodo<T> nodo =	null;
+		nodo = obterNodoPosicao(posicao);
+		return nodo.conteudo;
+
 	}
 
 	@Override
 	public int obterTamanho() {
-		
+
 		int resultado = 0;
-		Nodo<T> nodo = null;
-		
-		nodo = inicio;
-		while (nodo.proximo != null){
+		Nodo<T> nodo =	null;
+		nodo =	inicio;
+		while (nodo.proximo != null) {
 			nodo = nodo.proximo;
 			resultado++;
+
 		}
-		
+
 		return resultado;
+
 	}
 
 	@Override
 	public T removerPosicao(int posicao) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Nodo<T> nodo =	null;
+		Nodo<T> anterior =	new Nodo<T>();
+		nodo = obterNodoPosicao(posicao);
+		anterior = obterNodoPosicao(posicao - 1);
+		anterior.proximo = nodo.proximo;
+		nodo.proximo = null;
+		T resultado = nodo.conteudo;
+		return resultado;
+		
 	}
 
 	@Override
 	public void esvaziar() {
-		// TODO Auto-generated method stub
-		
+
+		inicio.proximo = null;
+
+	}
+
+	private Nodo<T> obterNodoPosicao(int posicao) {
+
+		Nodo<T> nodo = inicio;
+		int indice = -1;
+		while (posicao != indice) {
+			nodo = nodo.proximo;
+			indice++;
+		}
+		return nodo;
+
 	}
 
 }
