c4265031c03b33d62cebb8bcd728e365ec2ce3f9
631220002 <631220002@Sala603-20.fspoaeduc.com.br>
2013-06-17 21:42:56 -0300

    Trabalho 13

--- entregas/trabalho13/src/main/java/br/com/senacrs/alp/aulas/Factory.java
+++ entregas/trabalho13/src/main/java/br/com/senacrs/alp/aulas/Factory.java
@@ -1,21 +1,17 @@
 package br.com.senacrs.alp.aulas;
 
 public class Factory {
-	
 	private static final Factory instancia = new Factory();
-	
+
 	private Factory() {
 	}
-	
+
 	public ValidarRequisicaoGet criarValidacao() {
-	
 		ValidarRequisicaoGet resultado = null;
-		
-		//implementar
-		
+		resultado = new MinhaValidacao();
 		return resultado;
 	}
-	
+
 	public static Factory getInstancia() {
 		return instancia;
 	}
--- entregas/trabalho13/src/main/java/br/com/senacrs/alp/aulas/ValidarRequisicaoGet.java
+++ entregas/trabalho13/src/main/java/br/com/senacrs/alp/aulas/ValidarRequisicaoGet.java
@@ -3,8 +3,7 @@ package br.com.senacrs.alp.aulas;
 import java.io.Reader;
 
 public interface ValidarRequisicaoGet {
-	
 	public static final String NOVA_LINHA = "\r\n";
-	
+
 	boolean ehRequisicaoGetValida(Reader requisicao);
 }
